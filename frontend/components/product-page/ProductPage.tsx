2bc1a7f28db5b770fade555511fff4e2686ba5d3
SeijaHuang <hsj12544975@gmail.com>
2024-09-25 11:34:47 +1000

    star rate completed

---
+++ frontend/components/product-page/ProductPage.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import perfume from "./assets/Image.png";
+import Link from "next/link";
+import { StarRating } from "./components/StarRating";
+
+export function ProductPage() {
+  return (
+    <section className="flex px-20">
+      <div className="w-1/2 h-1/2">
+        <Image src={perfume} alt="perfume" width={700} height={700} />
+      </div>
+      <div className="p-10 flex flex-col gap-8">
+        <h1>PERFUME</h1>
+        <div>
+          <h1>Sinsa Dong Gangnam-Gu Zara</h1>
+          <h3>SKU: E994-50-1 Category: For Him Tag: Men</h3>
+          <div className="flex gap-3">
+            <StarRating starNum={4} />
+            <Link href="/product" className="underline">
+              Read Review
+            </Link>
+          </div>
+
+          <h3>100ml / 3.4 oz</h3>
+          <span>
+            This item has
+            <Link href="/product" className="underline">
+              special conditions for returns
+            </Link>
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ frontend/components/product-page/components/StarRating.tsx
@@ -0,0 +1,101 @@
+export function StarRating({ starNum }: { starNum: number }) {
+  return <div className="flex gap-1">{Stars(starNum)}</div>;
+}
+
+function Stars(num: number) {
+  const totalNum = 5;
+  if (num % 1 === 0) {
+    const stars = Array.from({ length: totalNum }, (_, i) => i < num);
+    return stars.map((filled, i) =>
+      filled ? <FullStar key={i} /> : <EmptyStar key={i} />
+    );
+  }
+  if (num % 1 != 0) {
+    const stars = Array.from({ length: totalNum }, (_, i) => {
+      const int = Math.floor(num);
+      if (i < int) return { filled: true, half: false };
+      if (i === int) return { filled: false, half: true };
+      return { filled: false, half: false };
+    });
+    return stars.map((star) => {
+      return Star(star.filled, star.half);
+    });
+  }
+}
+
+function Star(filled: boolean, half: boolean) {
+  return (filled && <FullStar />) || (half && <HalfStar />) || <EmptyStar />;
+}
+
+type CommonStarProps = {
+  clip?: string;
+};
+
+function CommonStar({ clip }: CommonStarProps) {
+  return (
+    <>
+      <path
+        fillRule="evenodd"
+        d="M10 3l2.5 5.5 5.5.5-4 4 1 5.5-5-2-5 2 1-5.5-4-4 5.5-.5L10 3z"
+        clipRule="evenodd"
+        clip-path={clip}
+      />
+      <StarOutline />
+    </>
+  );
+}
+
+function StarOutline() {
+  return (
+    <path
+      fill="none"
+      d="M10 3l2.5 5.5 5.5.5-4 4 1 5.5-5-2-5 2 1-5.5-4-4 5.5-.5L10 3z"
+      stroke="#808274"
+      stroke-width="2"
+    />
+  );
+}
+
+function FullStar() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-5 w-5 text-[#808274]"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <CommonStar />
+    </svg>
+  );
+}
+
+function HalfStar() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-5 w-5 text-[#808274]"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <defs>
+        <clipPath id="halfClip">
+          <rect x="0" y="0" width="10" height="20" />
+        </clipPath>
+      </defs>
+      <CommonStar clip="url(#halfClip)" />
+    </svg>
+  );
+}
+
+function EmptyStar() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-5 w-5 text-[#808274]"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <StarOutline />
+    </svg>
+  );
+}
