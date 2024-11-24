23dafbf830b466102f4c077dc8cdbe235fcf49cb
SeijaHuang <hsj12544975@gmail.com>
2024-11-24 12:21:42 +1100

    updated the Image style attribute

--- frontend/components/homepage/membership/components/ProductPoint.tsx
+++ frontend/components/homepage/membership/components/ProductPoint.tsx
@@ -10,7 +10,7 @@ export function ProductPoint({ src, alt, description }: ProductPointProps) {
   return (
     <div className="flex flex-col xl:flex-row items-center justify-center text-center md:text-left gap-2">
       <div className="w-[20px] h-[20px] relative">
-        <Image src={src} alt={alt} fill objectFit="contain" />
+        <Image src={src} alt={alt} fill style={{ objectFit: "contain" }} />
       </div>
       <p className="text-white text-xs text-center xl:text-start font-semibold lg:text-sm">
         {description}
