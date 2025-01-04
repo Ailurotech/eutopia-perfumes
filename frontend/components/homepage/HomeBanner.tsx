dbfa8955bf5b9332a9ab6a43014af1681aa27ecb
JackyChn <ccc1020048076@gmail.com>
2025-01-05 04:53:09 +1030

    Fix: redirect homepage bg sound

--- frontend/components/homepage/HomeBanner.tsx
+++ frontend/components/homepage/HomeBanner.tsx
@@ -1,12 +1,8 @@
-import React from "react";
-import {
-  Splide,
-  SplideSlide,
-  SplideProps,
-  SplideTrack,
-} from "@splidejs/react-splide";
-import "@splidejs/react-splide/css/sea-green";
-import { Icon } from "../common/Icon";
+import React, { useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 import { VideoPlayer } from "../common/VideoPlayer";
 import { NavigationRoute, RoutRoute } from "../route";
 import { IVideo } from "@/interface/video";
@@ -16,14 +12,7 @@ interface HomeBannerProps {
 }
 
 const HomeBanner = ({ videos }: HomeBannerProps) => {
-  const options: SplideProps["options"] = {
-    type: "loop",
-    perPage: 1,
-    autoplay: true,
-    pauseOnHover: true,
-    resetProgress: false,
-    interval: 3000,
-  };
+  const swiperRef = useRef<any>(null);
 
   const matchPath = (path: string) => {
     switch (path) {
@@ -40,33 +29,31 @@ const HomeBanner = ({ videos }: HomeBannerProps) => {
 
   return (
     <div className="bg-[#F5E1C9] w-full p-0 sm:px-6 md:px-12 lg:px-28 2xl:px-32">
-      <Splide hasTrack={false} options={options} style={{ padding: "2rem 0" }}>
-        <SplideTrack>
-          {videos.map((item) => (
-            <SplideSlide key={item._id} className="flex justify-center">
-              <VideoPlayer
-                video={item}
-                page="home"
-                linkPath={matchPath(item.slug)}
-              />
-            </SplideSlide>
-          ))}
-        </SplideTrack>
-        <div className="splide__arrows !text-white !fill-white text-xs sm:text-2xl xl:text-4xl">
-          <button
-            className="splide__arrow splide__arrow--prev"
-            style={{ left: "2%" }}
+      <Swiper
+        modules={[Navigation, Autoplay]}
+        loop
+        autoplay={{ delay: 5000, disableOnInteraction: false }}
+        navigation
+        slidesPerView={1}
+        className="swiper-container"
+        style={{ padding: "2rem 0" }}
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
+      >
+        {videos.map((item) => (
+          <SwiperSlide
+            key={item._id}
+            className="flex justify-center"
+            onMouseEnter={() => swiperRef.current?.autoplay.stop()}
+            onMouseLeave={() => swiperRef.current?.autoplay.start()}
           >
-            <Icon name="arrow" style={{ fill: "white" }} />
-          </button>
-          <button
-            className="splide__arrow splide__arrow--next"
-            style={{ right: "2%" }}
-          >
-            <Icon name="arrow" style={{ fill: "white" }} />
-          </button>
-        </div>
-      </Splide>
+            <VideoPlayer
+              video={item}
+              page="home"
+              linkPath={matchPath(item.slug)}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
