3880761d89aef9c1a00db06c5714b5cef18c3251
JackyChn <ccc1020048076@gmail.com>
2025-01-06 23:39:36 +1030

    Fix: paddings, pagination bar, color

--- frontend/components/homepage/HomeBanner.tsx
+++ frontend/components/homepage/HomeBanner.tsx
@@ -1,8 +1,9 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Autoplay } from "swiper/modules";
+import { Navigation, Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
 import { VideoPlayer } from "../common/VideoPlayer";
 import { NavigationRoute, RoutRoute } from "../route";
 import { IVideo } from "@/interface/video";
@@ -31,12 +32,36 @@ const HomeBanner = ({ videos }: HomeBannerProps) => {
     <div className="bg-[#F5E1C9] w-full p-0 sm:px-6 md:px-12 lg:px-28 2xl:px-32 relative">
       <Swiper
         ref={swiperRef}
-        modules={[Navigation, Autoplay]}
+        modules={[Navigation, Autoplay, Pagination]}
         loop
         autoplay={{ delay: 5000, disableOnInteraction: false }}
         slidesPerView={1}
         className="swiper-container"
-        onSwiper={(swiper) => (swiperRef.current = swiper)}
+        onSwiper={(swiper) => {
+          swiperRef.current = swiper;
+          swiper.el.addEventListener("click", (event: any) => {
+            const target = event.target as HTMLElement;
+            if (target.dataset.index) {
+              const slideIndex = parseInt(target.dataset.index);
+              swiper.slideTo(slideIndex);
+            }
+          });
+        }}
+        pagination={{
+          clickable: true,
+          type: "custom",
+          renderCustom: (swiper, current, total) => {
+            let paginationBars = "";
+            for (let i = 1; i <= total; i++) {
+              paginationBars += `<span data-index="${i - 1}" class="${
+                i === current
+                  ? "bg-[#14b8a6] w-5 h-1.5 rounded-full"
+                  : "bg-gray-400 w-5 h-1.5 rounded-full opacity-50"
+              } inline-block mx-1 mb-3 cursor-pointer"></span>`;
+            }
+            return `<div class="flex justify-center items-center">${paginationBars}</div>`;
+          },
+        }}
         navigation={{
           prevEl: "#swiper-button-prev",
           nextEl: "#swiper-button-next",
@@ -45,7 +70,7 @@ const HomeBanner = ({ videos }: HomeBannerProps) => {
         {videos.map((item) => (
           <SwiperSlide
             key={item._id}
-            className="flex justify-center py-12"
+            className="flex justify-center p-12"
             onMouseEnter={() => swiperRef.current?.autoplay?.stop()}
             onMouseLeave={() => swiperRef.current?.autoplay?.start()}
           >
@@ -57,7 +82,6 @@ const HomeBanner = ({ videos }: HomeBannerProps) => {
           </SwiperSlide>
         ))}
       </Swiper>
-
       <div className="splide__arrows !text-white !fill-white text-xs sm:text-2xl xl:text-4xl absolute inset-0 flex justify-between items-center">
         <button
           id="swiper-button-prev"
