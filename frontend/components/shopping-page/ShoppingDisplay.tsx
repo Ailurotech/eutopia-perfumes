a00e5cd4c5ee5fe1c449e4f25733919ce5e22496
JackyChn <ccc1020048076@gmail.com>
2025-01-07 19:53:57 +1030

    Fix: two products per row for all, for-him, for-her, netural on mobile view

--- frontend/components/shopping-page/ShoppingDisplay.tsx
+++ frontend/components/shopping-page/ShoppingDisplay.tsx
@@ -68,11 +68,11 @@ export function ShoppingDisplay({
       )}
     >
       <div
-        className={clsx(
-          "grid grid-cols-4 grid-rows-[repeat(7,auto)]",
-          "gap-y-4 lg:gap-y-7 xl:gap-y-10 gap-x-4 md:gap-x-6 lg:gap-x-8 xl:gap-x-12 2xl:gap-x-20"
-        )}
+        className={
+          "grid grid-cols-4 gap-y-4 lg:gap-y-7 xl:gap-y-10 gap-x-4 md:gap-x-6 lg:gap-x-8 xl:gap-x-12 2xl:gap-x-20"
+        }
       >
+        {/* Filter settings*/}
         <div className="col-span-4">
           <div className="flex justify-between gap-4 lg:gap-10 xl:gap-16 flex-wrap">
             {filterLists.map((list) => (
@@ -86,6 +86,7 @@ export function ShoppingDisplay({
             ))}
           </div>
         </div>
+        {/* Filter labels */}
         <div className="col-span-4">
           <div className="flex justify-start gap-x-4 lg:gap-x-8 flex-wrap gap-y-2">
             {hasFilters &&
@@ -100,16 +101,22 @@ export function ShoppingDisplay({
               )}
           </div>
         </div>
+        {/* Spinner */}
         {!displayProducts.length && !hasFilters && (
           <div className="col-span-4">
             <LoadingSpinner />
           </div>
         )}
+        {/* No products found */}
         {!displayProducts.length && hasFilters && (
           <div className="col-span-4 flex justify-center">
             <h1 className="text-3xl font-black">No products found</h1>
           </div>
         )}
+      </div>
+
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-y-4 lg:gap-y-7 xl:gap-y-10 gap-x-4 md:gap-x-6 lg:gap-x-8 xl:gap-x-12 2xl:gap-x-20">
+        {/* IndividualProducts */}
         {displayProducts.map((product, index) => (
           <IndividualProductForShoppingPage
             key={index}
@@ -124,6 +131,8 @@ export function ShoppingDisplay({
           />
         ))}
       </div>
+
+      {/* Pagination */}
       <Pagination
         maxPage={Math.ceil(filteredProducts.length / displayNum)}
         currentPage={currentPage}
