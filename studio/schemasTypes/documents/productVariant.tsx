f5d2249ed1e0c14cf23a53d5255c0135822a9fb2
JackyChn <ccc1020048076@gmail.com>
2025-01-22 03:58:08 +1030

    Add: getBestSellersProductQuery && getNewArrivalsProductQuery in query, show them on homepage

--- studio/schemasTypes/documents/productVariant.tsx
+++ studio/schemasTypes/documents/productVariant.tsx
@@ -31,6 +31,19 @@ export const productVariantType = defineType({
       options: {field: 'store.title'},
     }),
     defineField({
+      name: 'MarkedAs',
+      type: 'number',
+      title: 'Marked As',
+      description: 'Choose among General, Bestseller or New Arrivals',
+      options: {
+        list: [
+          {title: 'General', value: 0},
+          {title: 'Bestseller', value: 1},
+          {title: 'New Arrivals', value: 2},
+        ],
+      },
+    }),
+    defineField({
       name: 'store',
       title: 'Shopify',
       description: 'Variant data from Shopify (read-only)',
