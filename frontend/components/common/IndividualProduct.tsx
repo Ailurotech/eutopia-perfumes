e424075bf390b8a14981386e1964029856d111c4
feixiang <913737079@qq.com>
2025-02-19 15:59:40 +1100

    refactor clickHandler func

--- frontend/components/common/IndividualProduct.tsx
+++ frontend/components/common/IndividualProduct.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import { storeProductToLocal } from "@/utils/local-storage-for-product";
 import React from "react";
 import { createSlug } from "@/utils/slug";
+import { NextRouter } from "next/router";
 
 interface IndividualProductProps {
   image: string;
@@ -19,6 +20,23 @@ interface IndividualProductProps {
 
 const literata = Literata({ weight: "700", subsets: ["latin"] });
 const splitOperator = "|";
+
+function clickHandler(
+  e: React.MouseEvent<HTMLElement>,
+  title: string,
+  router: ReturnType<typeof useRouter>
+) {
+  if (!title) return;
+  const slug = createSlug(title);
+  const target = e.target as HTMLElement;
+  if (
+    target.tagName === "IMG" ||
+    target.tagName === "H5" ||
+    target.tagName === "H2"
+  ) {
+    router.push(`/product/${slug}`);
+  }
+}
 
 export function IndividualProductForShoppingPage({
   image,
@@ -35,23 +53,10 @@ export function IndividualProductForShoppingPage({
     ? title.split(splitOperator).slice(0, 2)
     : [title];
 
-  function clickHandler(e: React.MouseEvent<HTMLElement>, id: number) {
-    if (!title) return;
-    const slug = createSlug(title);
-    const target = e.target as HTMLElement;
-    if (
-      target.tagName === "IMG" ||
-      target.tagName === "H5" ||
-      target.tagName === "H2"
-    ) {
-      router.push(`/product/${slug}`);
-    }
-  }
-
   return (
     <div
       className="grid grid-rows-[repeat(4,auto)] items-center justify-center group gap-1 md:gap-2 2xl:gap-4 cursor-pointer text-center"
-      onClick={(e) => id && clickHandler(e, id)}
+      onClick={(e) => id && clickHandler(e, title, router)}
     >
       <div className="w-full aspect-[23/30] relative rounded-xl">
         <Image src={image} alt={title} className="object-contain" fill />
@@ -114,28 +119,11 @@ export function IndividualProductForProductPage({
     ? title.split(splitOperator).slice(0, 1)
     : [title];
   const router = useRouter();
-  function clickHandler(e: React.MouseEvent<HTMLElement>, id: number) {
-    if (!title) return;
-    const slug = createSlug(title);
-    console.log("Navigating to product with:", {
-      originalTitle: title,
-      generatedSlug: slug,
-      id,
-    });
-    const target = e.target as HTMLElement;
-    if (
-      target.tagName === "IMG" ||
-      target.tagName === "H5" ||
-      target.tagName === "H2"
-    ) {
-      router.push(`/product/${slug}`);
-    }
-  }
 
   return (
     <div
       className="flex flex-col items-center cursor-pointer"
-      onClick={(e) => id && clickHandler(e, id)}
+      onClick={(e) => id && clickHandler(e, title, router)}
     >
       <div className="h-64 aspect-[23/30] relative">
         <Image src={image} alt={title} className="object-contain" fill />
